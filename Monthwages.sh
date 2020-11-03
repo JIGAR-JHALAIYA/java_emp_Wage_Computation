80f9be75f88df55e4cd738212be19f7bdb552eff
JIGAR-JHALAIYA <jdjhalaiya27@gmail.com>
2020-11-03 10:49:36 +0530

    Calculating_Month_Wages

---
+++ Monthwages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+public class HelloWorld{
+
+     public static void main(String []args)
+     {
+        System.out.println("Welcome to Employee Wage computation");
+        int wagesPerHour=20;
+        int dayperHour=8;
+        int  daypermonth=20
+        int monthEarning= dayperHour * dayPermonth* wagesPerHour;
+        System.out.println(monthEarning);
+     }
+}
+
